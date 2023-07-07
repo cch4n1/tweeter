bcdca1ce72e7d9ef2e9da500957d5d7f937c999d
Charlie C <zbyabqrc@pm.me>
2023-07-07 19:26:01 +0000

    character counter js file targets the dom using jquery and updates the remaining characters left in the text box. Updated to show max text length before input. Updated index.html to show counter dynamically on webpage. Updated character counter js file to add new css class if counter goes negative and removes class if counter is positive. Created new matching 'negative' css class to turn text red. Counter now turns red if it goes into negative and changes back to default color if positive.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,22 @@
 
 $(document).ready(function() {
   // --- our code goes here ---
+  const counter = $('#counter-placeholder');
+
   $('#tweet-text').on('input', function() {
-    var text = $(this).val();
-    var remainingChars = 140 - text.length;
+    const text = $(this).val();
+    const remainingChars = 140 - text.length;
     $('#counter').text(remainingChars);
     console.log("Charaters remaining:", remainingChars);
+    counter.text(remainingChars);
+
+    if (remainingChars < 0) {
+      counter.addClass('negative');
+    } else {
+      counter.removeClass('negative');
+    }
   });
+
+    // Trigger the input event initially to update the counter on page load
+    $('#tweet-text').trigger('input');
 });
